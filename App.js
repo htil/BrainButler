a6375e3f7b1f1c6aa17d94f6c01be374f99bbffb
elmines <elmines325@gmail.com>
2018-09-27 15:58:26 -0500

    Uninstall react-native-libmuse

--- App.js
+++ App.js
@@ -2,31 +2,7 @@
 import React from "react";
 import {Button, View, ScrollView, Text, DeviceEventEmitter} from "react-native";
 import {createStackNavigator} from "react-navigation";
-
-import LibMuse from "react-native-libmuse";
-
 import ArticleScreen from "./ArticleScreen";
-
-
-
-LibMuse.Init();
-LibMuse.StartSearch();
-console.log("Started searching for Muse devices. . .");
-
-DeviceEventEmitter.addListener("OnChangeMuseList", args => {
-	console.log("The Muse List changed.");
-	let [museList] = args;
-	if (museList.length > 0)
-	{
-		LibMuse.Connect();
-		console.log("Trying to connect to a detected Muse device. . .");
-	}
-});
-
-DeviceEventEmitter.addListener("OnReceiveMuseDataPacket", args => {
-	let [type, channelValues] = args;
-	console.log(`Type: ${type} ChannelValues: ${JSON.stringify(channelValues)}`);
-});
 
 
 class MenuScreen extends React.Component
