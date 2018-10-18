3ce42bdb712dcbd38d1914377fbc3bd49663f7e4
Ethan Mines <elmines325@gmail.com>
2018-10-18 17:11:19 -0500

    Make yes/no buttons for training game

--- GameScreen.js
+++ GameScreen.js
@@ -1,22 +1,64 @@
 "use strict";
 import React from "react";
-import {View, Text, DeviceEventEmitter} from "react-native";
-import MuseBanner from "./MuseBanner";
+import {View, Text, StyleSheet, DeviceEventEmitter} from "react-native";
+import {TouchableNativeFeedback} from "react-native";
+
+function confirmCorrect()
+{
+  DeviceEventEmitter.emit("ArtificialGood");
+}
+function confirmWrong()
+{
+  DeviceEventEmitter.emit("ArtificialBad");
+}
 
 export default class GameScreen extends React.Component
 {
   render()
   {
-    var museConnected = this.props.navigation.getParam("museConnected", false);
-    if (!museConnected)
-    {
-      return (
-        <View><Text>Go back to the main menu and connect a Muse first.</Text></View>
-      );
-    }
-
+    //var museConnected = this.props.navigation.getParam("museConnected", false);
     return (
-      <View><Text>We are going to play a simple gave to train BrainButler.</Text></View>
+      <View style={{flex: 1}}>
+        <View style={{flex: 4}}>
+          <Text>We are going to play a game</Text>
+        </View>
+        <View style={{flex: 1, flexDirection: "row"}}>
+          <ChoiceButton text="Right!" backgroundColor="green" onPress={confirmCorrect}/>
+          <ChoiceButton text="Wrong!" backgroundColor="red" onPress={confirmWrong}/>
+        </View>
+      </View>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+  choiceButton:
+  {
+    flex: 1,
+    backgroundColor: "blue",
+  },
+  choiceText:
+  {
+    textAlign: "center",
+    color: "white",
+    fontSize: 20
+  }
+});
+
+class ChoiceButton extends React.Component
+{
+  //Props: onPress, backgroundColor, text
+  render()
+  {
+    const backgroundColor = this.props.backgroundColor;
+    const viewStyle = {...styles.choiceButton, ...{backgroundColor} };
+    return (
+      <TouchableNativeFeedback onPress={this.props.onPress}>
+        <View style={viewStyle}>
+          <Text style={styles.choiceText}>{this.props.text}</Text>
+        </View>
+      </TouchableNativeFeedback>
     );
   }
 }
