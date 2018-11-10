09e7e476014ba81830da3e4b8b8edb9f99f6ff7c
Ethan Mines <elmines325@gmail.com>
2018-11-10 17:05:24 -0600

    Rotate screen programmatically

--- ArticleScreen.js
+++ ArticleScreen.js
@@ -1,17 +1,58 @@
-"use strict";
+//@flow
 import React from "react";
-import {ScrollView, Text} from "react-native";
+import {View, ScrollView, Text, TouchableNativeFeedback, StyleSheet} from "react-native";
 import MuseBanner from "./MuseBanner";
+import Orientation from "react-native-orientation";
 
 export default class ArticleScreen extends React.Component
 {
+	constructor(props)
+	{
+		super(props);
+		this.rotate = (): void => {
+			Orientation.getOrientation((err, orientation) => {
+				if (orientation == "LANDSCAPE") Orientation.lockToPortrait();
+				else                            Orientation.lockToLandscape();
+				Orientation.unlockAllOrientations();
+			});
+		}
+	}
 	render()
 	{
 		const {text} = this.props.navigation.getParam("text", "");
 		return (
-			<ScrollView>
-				<Text>{text}</Text>
-			</ScrollView>
+			<View style={{flex:1}}>
+  			<ScrollView style={{flex:1}}>
+  				<Text>{text}</Text>
+  			</ScrollView>
+  			<Button text="Rotate the Screen!" onPress={this.rotate}/>
+			</View>
 		);
 	}
 }
+
+class Button extends TouchableNativeFeedback
+{
+  //Props: onPress, text
+  render()
+  {
+    return (
+      <TouchableNativeFeedback style={{flex: 1}} onPress={this.props.onPress}>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>{this.props.text}</Text>
+        </View>
+      </TouchableNativeFeedback>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+	button: {
+		backgroundColor: "blue",
+	},
+	buttonText: {
+		color: "white",
+		fontSize: 60,
+		textAlign: "center"
+	}
+});
--- android/app/src/main/java/com/brainbutler/MainActivity.java
+++ android/app/src/main/java/com/brainbutler/MainActivity.java
@@ -5,12 +5,25 @@ import com.facebook.react.ReactActivity;
 //Set the module's activity here
 import com.reactlibrary.RNLibMuseModule;
 
+//For configuring react-native-orientation
+import android.content.Intent;
+import android.content.res.Configuration;
+
 public class MainActivity extends ReactActivity {
 
     public MainActivity()
     {
       super();
       RNLibMuseModule.mainActivity = this;
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig)
+    {
+      super.onConfigurationChanged(newConfig);
+      Intent intent = new Intent("onConfigurationChanged");
+      intent.putExtra("newConfig", newConfig);
+      this.sendBroadcast(intent);
     }
 
     /**
