dccdd5dc2e6d670062d30db42852df8041554b3f
Ethan Mines <elmines325@gmail.com>
2019-01-20 17:07:27 -0600

    WIP: Set up interface for RL agent actions/rewards

---
+++ Agent.js
@@ -0,0 +1,29 @@
+//@flow
+import {Alert} from "react-native";
+
+import SystemSetting from "react-native-system-setting";
+
+import Config from "./Config";
+
+const title: String = "Hey!";
+const brightenMessage: String =
+  "It seemed like you were still looking, so I brightened the screen!";
+
+
+function positiveResponse() {}
+function negativeResponse() {}
+
+function brightenScreen() {
+	 SystemSetting.getAppBrightness().then((curr) => {
+		 SystemSetting.setAppBrightness(Config.initialBrightness);
+
+     Alert.alert(title, brightenMessage,
+       [
+         {text: "Thanks", onPress: positiveResponse},
+         {text: "No Thanks", onPress: negativeResponse}
+       ]
+     );
+	 });
+}
+
+export default {brightenScreen};
