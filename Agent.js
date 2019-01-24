0d648c47f90de5f0eabff958f94d4daaf1c6d30a
Ethan Mines <elmines325@gmail.com>
2019-01-24 17:43:10 -0600

    Handle data streaming and RL actions in Agent instead of ArticleScreen

--- Agent.js
+++ Agent.js
@@ -1,29 +1,142 @@
 //@flow
 import {Alert} from "react-native";
 
+import Orientation from "react-native-orientation";
 import SystemSetting from "react-native-system-setting";
 
 import Config from "./Config";
+import BBSocket from "./BBSocket";
+import {eegObservable} from "./Streaming";
+
+
+export default class Agent {
+
+  constructor() {
+		this.eegBuffer = [];
+		this.subscriptions = [];
+		this.callbackIds = [];
+
+		this.socket = Agent._openSocket();
+
+		this.orientListener = (orientation: String): void => {
+        orientation = orientation.toLowerCase();
+				this.socket.send(JSON.stringify({type: "event",
+          body: {eventName: "rotation", orientation, timestamp: Date.now()
+        }}));
+		};
+		Orientation.addOrientationListener(this.orientListener);
+
+		this.subscriptions.push( eegObservable.subscribe((packet) => {
+			this.eegBuffer.push(packet);
+			if (Config.sampleFrequency <= this.eegBuffer.length)
+			{
+				this.socket.send(JSON.stringify({type: "data", body: this.eegBuffer}));
+				this.eegBuffer = [];
+			}
+		}));
+
+ 		this.subscriptions.push(this.socket.actions().subscribe((action) => {
+			console.log("Received action "+action);
+			if      (action == "brighten") this._brighten();
+			else if (action == "rotate")   this._rotate();
+		}));
+
+		this.callbackIds.push( setInterval(() => {this._darkenScreen()}, 10000) );
+  } //End constructor
+
+  destructor() {
+		Orientation.removeOrientationListener(this.orientListener);
+
+		this.subscriptions.forEach(subscription => subscription.unsubscribe());
+		this.subscriptions = [];
+
+		this.callbackIds.forEach(callbackId => clearInterval(callbackId));
+		this.callbackIds = [];
+
+    this.socket.send(JSON.stringify({type: "eof"}));
+		this.socket.close();
+  }
+
+  static _openSocket() {
+    const socket = BBSocket.getInstance();
+		socket.open(() => {
+			socket.send(JSON.stringify({type: "header", body: {}}));
+
+			Orientation.getOrientation((err, orientation) => {
+        orientation = orientation.toLowerCase();
+				socket.send(JSON.stringify({
+					type: "event",
+					body: {eventName: "orientation", orientation, timestamp: Date.now()}
+				}));
+			});
+
+			SystemSetting.getAppBrightness().then((curr) => {
+        const brightness = brightnessSetting(curr);
+				socket.send(JSON.stringify({
+					type: "event",
+					body: {eventName: "brightness", brightness, timestamp: Date.now()}
+				}));
+			});
+		});
+    return socket;
+  }
+
+	_darkenScreen() {
+			SystemSetting.getAppBrightness().then((curr) =>{
+
+				const newSetting = brightnessSetting(curr);
+				const newValue = Config.brightness[newSetting];
+
+				if ( (newValue < curr) && (Math.abs(newValue - curr) > 0.001) )
+					this.socket.send(JSON.stringify({type: "event",
+						body: {eventName: "brightness", timestamp: Date.now(), brightness: newSetting}
+					}));
+
+  			SystemSetting.setAppBrightness(newValue);
+			});
+	}
+
+  _rotate() {
+	  Orientation.getOrientation((err, orientation) => {
+		  if (orientation == "LANDSCAPE") Orientation.lockToPortrait();
+		  else                            Orientation.lockToLandscape();
+		  Orientation.unlockAllOrientations();
+      this._notify(title, rotateMessage);
+    });
+  }
+
+
+  _brighten() {
+	   SystemSetting.getAppBrightness().then((curr) => {
+		     SystemSetting.setAppBrightness(Config.brightness.full);
+         this._notify(title, brightenMessage);
+	   });
+  }
+
+  _notify(title, message) {
+     Alert.alert(title, message,[
+         {text: "Thanks",    onPress: () => {this._thank();} },
+         {text: "No Thanks", onPress: () => {this._noThank();} }
+     ]);
+   }
+
+   _thank() {
+
+   }
+
+   _noThank() {
+
+   }
+} //End class
+
+function brightnessSetting(value) {
+				if      (value == Config.brightness.full)   return "medium";
+				else if (value == Config.brightness.medium) return "low";
+				return "low";
+}
 
 const title: String = "Hey!";
 const brightenMessage: String =
   "It seemed like you were still looking, so I brightened the screen!";
-
-
-function positiveResponse() {}
-function negativeResponse() {}
-
-function brightenScreen() {
-	 SystemSetting.getAppBrightness().then((curr) => {
-		 SystemSetting.setAppBrightness(Config.brightness.full);
-
-     Alert.alert(title, brightenMessage,
-       [
-         {text: "Thanks", onPress: positiveResponse},
-         {text: "No Thanks", onPress: negativeResponse}
-       ]
-     );
-	 });
-}
-
-export default {brightenScreen};
+const rotateMessage: String =
+  "It seemed like you wanted the screen this way!";
--- ArticleScreen.js
+++ ArticleScreen.js
@@ -1,18 +1,12 @@
 //@flow
 import React from "react";
 import {View, ScrollView, Text, TouchableNativeFeedback, StyleSheet} from "react-native";
-import {Alert} from "react-native";
 import Orientation from "react-native-orientation";
 import SystemSetting from "react-native-system-setting";
 
-import {merge} from "rxjs";
-
 import Agent from "./Agent";
 import Config from "./Config.js";
-import BBSocket from "./BBSocket.js";
-import MuseBanner from "./MuseBanner";
 import Styles from "./Styles";
-import {eegObservable, accObservable, gyroObservable} from "./Streaming.js";
 
 type Props = {};
 type State = {orientation: String};
@@ -25,88 +19,13 @@ export default class ArticleScreen extends React.Component<Props, State>
 		this.state = {orientation: Orientation.getInitialOrientation()};
     SystemSetting.setAppBrightness(Config.initialBrightness);
 
-		this.eegBuffer = [];
-		this.subscriptions = [];
-		this.callbackIds = [];
-
-		this.socket = BBSocket.getInstance();
-
- 		this.subscriptions.push(this.socket.actions().subscribe((action) => {
-			console.log("Received action "+action);
-			if (action == "brighten"){
-					Agent.brightenScreen();
-			}
-		}));
-
-		this.socket.open(() => {
-			this.socket.send(JSON.stringify({type: "header", body: {}}));
-
-			Orientation.getOrientation((err, orientation) => {
-				this.socket.send(JSON.stringify({
-					type: "event",
-					body: {eventName: "rotation", orientation, timestamp: Date.now()}
-				}));
-			});
-
-			SystemSetting.getAppBrightness().then((curr) => {
-				this.socket.send(JSON.stringify({
-					type: "event",
-					body: {eventName: "brightness", brightness: curr, timestamp: Date.now()}
-				}));
-			});
-
-		});
-
+		this.agent = new Agent();
 		this.orientListener = (orientation: String): void => {
-				const timestamp = Date.now();
 				this.setState((prev: State): State => {
 						return {orientation: orientation};
 				});
-				this.socket.send(JSON.stringify(
-					{type: "event", body: {eventName: "rotation", orientation, timestamp}}
-				));
 		};
 		Orientation.addOrientationListener(this.orientListener);
-
-		this.subscriptions.push( eegObservable.subscribe((packet) => {
-			this.eegBuffer.push(packet);
-			if (Config.sampleFrequency <= this.eegBuffer.length)
-			{
-				this.socket.send(JSON.stringify({type: "data", body: this.eegBuffer}));
-				this.eegBuffer = [];
-			}
-		}));
-
-		this.callbackIds.push( setInterval(() => {this.darkenScreen()}, 5000) );
-
-	}
-
-	static rotate() {
-			Orientation.getOrientation((err, orientation) => {
-				if (orientation == "LANDSCAPE") Orientation.lockToPortrait();
-				else                            Orientation.lockToLandscape();
-				Orientation.unlockAllOrientations();
-			});
-
-	}
-
-	darkenScreen() {
-			SystemSetting.getAppBrightness().then((curr) =>{
-
-				let newSetting = curr;
-				if (curr == Config.brightness.full)
-					newSetting = Config.brightness.medium;
-				else if (curr == Config.brightness.medium)
-					newSetting = Config.brightness.low;
-
-				if ( newSetting < curr ) {
-					const timestamp = Date.now();
-					this.socket.send(JSON.stringify(
-						{type: "event", body: {eventName: "brightness", timestamp, brightness: newSetting}}
-					));
-				}
-  			SystemSetting.setAppBrightness(newSetting);
-			});
 	}
 
 	awkParagraphs(text: string)
@@ -155,14 +74,7 @@ export default class ArticleScreen extends React.Component<Props, State>
 	componentWillUnmount()
 	{
 		Orientation.removeOrientationListener(this.orientListener);
-		this.subscriptions.forEach(subscription => subscription.unsubscribe());
-		this.subscriptions = [];
-
-		this.callbackIds.forEach(callbackId => clearInterval(callbackId));
-		this.callbackIds = [];
-
-    this.socket.send(JSON.stringify({type: "eof"}));
-		this.socket.close();
+		this.agent.destructor();
 
 		this.props.navigation.state.params.refresh();
 	}
